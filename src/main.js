4e7e66a6f7af9bc477d255f56f15ab278d75d857
Alberico Fagundes <alberico@fagundes.live>
2023-07-23 18:31:45 -0300

    criado rotas

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import * as VueRouter from "vue-router";
+import ShoppingCartPage from "./pages/ShoppingCartPage.vue";
+import ProductsPage from "./pages/ProductsPage.vue";
+import ProductsDetailPage from "./pages/ProductsDetailPage.vue";
 
-createApp(App).mount('#app')
+createApp(App)
+  .mount("#app")
+  .use(
+    VueRouter.createRouter({
+      history: VueRouter.createWebHistory(process.env.BASE_URL),
+      routes: [
+        {
+          path: "/cart",
+          component: ShoppingCartPage,
+        },
+        {
+          path: "/",
+          component: ProductsPage,
+        },
+        {
+          path: "/products/:productsId",
+          component: ProductsDetailPage,
+        },
+      ],
+    })
+  );
