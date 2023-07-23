b6924d976f0c9ad87186c5e311a0eac931d1091e
Alberico Fagundes <alberico@fagundes.live>
2023-07-23 18:54:44 -0300

    pagina produtos

---
+++ src/pages/ProductsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Produtos</h1>
+  <div class="grid-wrap">
+    <div v-for="product in products"></div>
+  </div>
+</template>
+
+<script>
+import { products } from "../temp-data";
+export default {
+  name: "ProductsPage",
+};
+</script>
